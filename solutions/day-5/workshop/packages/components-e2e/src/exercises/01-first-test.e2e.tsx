71578faacaff7deb87896fe9bbd4102e2f1aa0d3
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-12 21:02:47 +0100

    add first e2e test using dom-testing-library

--- solutions/day-5/workshop/packages/components-e2e/src/exercises/01-first-test.e2e.tsx
+++ solutions/day-5/workshop/packages/components-e2e/src/exercises/01-first-test.e2e.tsx
@@ -3,12 +3,22 @@
 import * as React from "react";
 import { mount } from "cypress-react-unit-test";
 import { View } from "@workshop/components/previous/02-context";
+import * as dtl from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
 
 describe("01-first-test", () => {
   /**
    * Exercise 1
    */
-  it("should render the View component", () => {
+  it("should render the View component", async () => {
     mount(<View />);
+
+    const input = await dtl.findByTestId(document.body, "view-input");
+    const add = await dtl.findByText(document.body, /add/i);
+
+    userEvent.type(input, "Hello");
+    userEvent.click(add);
+
+    await dtl.findByText(document.body, /first: hello/i, {}, { timeout: 5000 });
   });
 });
