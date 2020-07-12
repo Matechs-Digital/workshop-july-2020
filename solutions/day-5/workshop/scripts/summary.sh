a568552b4e6f49adb092e7322a4ed96cd35d50b9
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-12 21:51:44 +0100

    use rimraf

--- solutions/day-5/workshop/scripts/summary.sh
+++ solutions/day-5/workshop/scripts/summary.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-rm -rf summary
-rm -rf coverage
+rimraf summary
+rimraf coverage
 mkdir summary
 cp packages/common/coverage/coverage-final.json summary/common.json
 cp packages/components/coverage/coverage-final.json summary/components.json
 cp packages/components-e2e/coverage/coverage-final.json summary/components-e2e.json
 cp packages/app/coverage/coverage-final.json summary/app.json
 nyc -t ./summary report --reporter=html
-rm -rf summary
+rimraf summary
