0ee1710acf1a5c06fa5798bc4bb809298874972c
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-13 22:15:41 +0100

    improve webpack config

--- solutions/day-5/workshop/packages/components/webpack.config.js
+++ solutions/day-5/workshop/packages/components/webpack.config.js
@@ -1,11 +1,5 @@
-const path = require("path");
-
 module.exports = {
     resolve: {
-        alias: {
-            '@workshop/components': path.resolve("../components/src"),
-            '@workshop/common': path.resolve("../common/src")
-        },
         extensions: [".tsx", ".ts", ".js", ".jsx"],
     },
     module: {
