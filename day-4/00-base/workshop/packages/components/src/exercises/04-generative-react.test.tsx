980618a1eefdda6cbcf50fe49daa559ee7360a5b
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-15 20:33:49 +0100

    unify 04

--- day-4/00-base/workshop/packages/components/src/exercises/04-generative-react.test.tsx
+++ day-4/00-base/workshop/packages/components/src/exercises/04-generative-react.test.tsx
@@ -1,29 +1,41 @@
 import * as React from "react";
-import { cleanup, render, fireEvent } from "@testing-library/react";
+import * as RTL from "@testing-library/react";
 import fc from "fast-check";
 import { RemConverter } from "./04-generative-react";
 
-afterEach(cleanup);
+afterEach(RTL.cleanup);
 
 describe("04-generative-react", () => {
   /**
    * Exercise 1 (2 mins)
    */
   it("should render correctly", () => {
-    expect(render(<RemConverter />)).toBeDefined();
+    expect(RTL.render(<RemConverter />)).toBeDefined();
   });
 
   /**
    * Exercise 2 (5 mins)
    */
+  it("should convert px to the right rem value", async () => {
+    const { getByTestId, getByText } = RTL.render(<RemConverter />);
+    RTL.fireEvent.change(getByTestId("px"), {
+      target: { value: "32" },
+    });
+    RTL.fireEvent.click(getByText("Convert"));
+    expect((getByTestId("rem") as HTMLInputElement).value).toBe("2");
+  });
+
+  /**
+   * Exercise 3 (5 mins)
+   */
   it("should convert px to rem using property based assertion", async () => {
-    const { getByTestId, getByText } = render(<RemConverter />);
+    const { getByTestId, getByText } = RTL.render(<RemConverter />);
     fc.assert(
       fc.property(fc.nat(), fc.constant(16), (px, baseFontSize) => {
-        fireEvent.change(getByTestId("px"), {
+        RTL.fireEvent.change(getByTestId("px"), {
           target: { value: `${px}` },
         });
-        fireEvent.click(getByText("Convert"));
+        RTL.fireEvent.click(getByText("Convert"));
         expect((getByTestId("rem") as HTMLInputElement).value).toBe(
           `${px / baseFontSize}`
         );
